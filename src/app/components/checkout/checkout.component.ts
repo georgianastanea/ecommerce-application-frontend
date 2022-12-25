97dee934806e76939b7f14524eef007915e636bd
georgianastanea9 <109727380+georgianastanea9@users.noreply.github.com>
2022-12-25 22:00:20 +0200

    commit

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
+import { CartService } from 'src/app/services/cart.service';
 import { EcommerceFormService } from 'src/app/services/ecommerce-form.service';
 
 @Component({
@@ -27,14 +28,19 @@ export class CheckoutComponent implements OnInit {
 
 
   constructor(private formBuilder: FormBuilder,
-              private ecommerceFormService: EcommerceFormService) { }
+              private ecommerceFormService: EcommerceFormService,
+              private cartService: CartService) { }
 
   ngOnInit(): void {
+
+    this.reviewCartDetails();
+    
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName:[''],
-        lastName: [''],
-        email: ['']
+        firstName: new FormControl('',[Validators.required,Validators.minLength(2)]),
+        lastName: new FormControl('',[Validators.required,Validators.minLength(2)]),
+        email: new FormControl('',
+                              [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -83,6 +89,30 @@ export class CheckoutComponent implements OnInit {
 
   }
 
+  reviewCartDetails(){
+
+    this.cartService.totalQuantity.subscribe(
+     totalQuantity => this.totalQuantity = totalQuantity
+    );
+
+    this.cartService.totalPrice.subscribe(
+      totalPrice => this.totalPrice = totalPrice
+     );
+
+  }
+
+  get firstName(){
+    return this.checkoutFormGroup.get('customer.firstName');
+  }
+
+  get lastName(){
+    return this.checkoutFormGroup.get('customer.lastName');
+  }
+
+  get email(){
+    return this.checkoutFormGroup.get('customer.email');
+  }
+
   copyShippingAddressToBillingAddress(event: any) {
     if(event.target.checked){
       this.checkoutFormGroup.controls['billingAddress']
@@ -122,6 +152,10 @@ export class CheckoutComponent implements OnInit {
     }
 
   onSubmit(){
+
+    if(this.checkoutFormGroup.invalid){
+      this.checkoutFormGroup.markAllAsTouched();
+    }
 
   }
 
