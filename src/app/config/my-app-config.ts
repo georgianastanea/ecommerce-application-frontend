5cff3b909d358fc18caf54dda3bffb66f8a74a32
georgianastanea9 <109727380+georgianastanea9@users.noreply.github.com>
2022-12-26 02:30:43 +0200

    okta integrated

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default  {
+
+    oidc:{
+        clientId: '0oa7r4ykyiqSHeDZE5d7',
+        issuer: 'https://dev-77099192.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
